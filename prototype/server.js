ba85582d4f7436b1d9d934cfe97c6488f409ba1e
Randi <rahandi@kcv.if.its.ac.id>
2018-12-11 00:18:37 +0700

    multiplayer

--- prototype/server.js
+++ prototype/server.js
@@ -1,8 +1,15 @@
-var http = require('http')
+var app = require('express')()
+var http = require('http').Server(app)
 var url = require('url')
 var fs = require('fs')
+var io = require('socket.io')(http)
 
-http.createServer(function (req, res) {
+var user = {
+  'right': 0,
+  'left': 0
+}
+
+app.get('*', function(req, res){
   var q = url.parse(req.url, true)
   var filename = '.' + q.pathname
   fs.readFile(filename, function (err, data) {
@@ -14,4 +21,43 @@ http.createServer(function (req, res) {
     res.write(data)
     return res.end()
   })
-}).listen(7777)
+});
+
+http.listen(7777, '0.0.0.0')
+
+io.on('connection', function(socket) {
+  handle_socket(socket)
+})
+
+function handle_socket(socket) {
+  if (user.right == 0) {
+    user.right = socket
+    var userid = 'right'
+    console.log('assigned to right')
+  }
+  else if (user.left == 0) {
+    user.left = socket
+    var userid = 'left'
+    console.log('assigned to left')
+  }
+  else {
+    socket.disconnect()
+    console.log('connection refused')
+  }
+
+  socket.on('keydown', function(data) {
+    handle_key(userid, data, 'keydown')
+  })
+
+  socket.on('keyup', function(data) {
+    handle_key(userid, data, 'keyup')
+  })
+}
+
+function handle_key(userid, data, behavior) {
+  data = {
+    'player_side': userid,
+    'key_code': data,
+  }
+  io.emit(behavior, data)
+}
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-var http = require('http')
+var app = require('express')()
+var http = require('http').Server(app)
 var url = require('url')
 var fs = require('fs')
+var io = require('socket.io')(http)
 
-http.createServer(function (req, res) {
+var user = {
+  'right': 0,
+  'left': 0
+}
+
+var score = {
+  'right': 0,
+  'left': 0
+}
+
+app.get('*', function(req, res){
   var q = url.parse(req.url, true)
   var filename = '.' + q.pathname
   fs.readFile(filename, function (err, data) {
@@ -14,4 +26,63 @@ http.createServer(function (req, res) {
     res.write(data)
     return res.end()
   })
-}).listen(7777)
+});
+
+http.listen(7777, '0.0.0.0')
+
+io.on('connection', function(socket) {
+  console.log('connected')
+  handle_socket(socket)
+})
+
+function handle_socket(socket) {
+  if (user.right == 0) {
+    user.right = socket
+    var userid = 'right'
+    console.log('assigned to right')
+  }
+  else if (user.left == 0) {
+    user.left = socket
+    var userid = 'left'
+    console.log('assigned to left')
+  }
+  else {
+    socket.disconnect()
+    console.log('connection refused')
+  }
+
+  socket.on('disconnect', function() {
+    user[userid] = 0
+    console.log(userid + ' disconnect')
+    return
+  })
+
+  socket.on('who am i', function(data) {
+    socket.emit('you are', userid)
+  })
+
+  socket.on('ball', function(data) {
+    io.emit('ball', data)
+  })
+
+  socket.on('score', function(data) {
+    score[data] += 1
+    io.emit('score', score)
+  })
+
+  socket.on('keydown', function(data) {
+    handle_key(userid, data, 'keydown')
+  })
+
+  socket.on('keyup', function(data) {
+    handle_key(userid, data, 'keyup')
+  })
+}
+
+function handle_key(userid, data, behavior) {
+  data = {
+    'player_side': userid,
+    'key_code': data,
+  }
+  io.emit(behavior, data)
+}
